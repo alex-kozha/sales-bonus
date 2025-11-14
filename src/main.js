ac1858f42223b0fb3fb87d7ea5e6c97117019327
Alexander Kozhahin <korsukov2009@gmail.com>
2025-11-14 23:21:22 +0300

    sd

--- src/main.js
+++ src/main.js
@@ -47,6 +47,8 @@ function analyzeSalesData(data, options) {
     !data ||
     !Array.isArray(data.sellers) ||
     data.sellers.length === 0 ||
+    !Array.isArray(data.products) ||
+    data.products.length === 0 ||
     !Array.isArray(data.purchase_records)
   ) {
     throw new Error("Некорректные входные данные");
